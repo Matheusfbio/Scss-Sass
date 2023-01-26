6641b506b8bace1138178087989c5f77710abf36
Matheus Fábio <fabiomatheus488@gmail.com>
2023-01-26 19:00:35 -0300

    Project Vite + Sass

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../src/Scss/style.css'
+
+function App() {
+  return (
+	<body>
+		<div className='div1'>
+			<h1>1</h1>
+		</div>
+
+		<div className='div2'>
+			<h1>2</h1>
+		</div>
+
+		<div className='div3'>
+			<h1>3</h1>
+		</div>
+	</body>
+	)
+}
+
+export default App
